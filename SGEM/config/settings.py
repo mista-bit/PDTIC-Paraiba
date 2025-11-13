a55b5a943357152c4259347d103c02b59149fd32
Pedro <pedroucn2007@gmail.com>
2025-11-13 18:29:16 -0300

    Correção de bug com autenticação

--- SGEM/config/settings.py
+++ SGEM/config/settings.py
@@ -134,6 +134,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",  # Alternativa de localhost
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
