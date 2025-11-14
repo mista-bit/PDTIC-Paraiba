ba9888f4f3cc4e5d19a2967719da69ac6ff56150
Humberto023 <betinho040805@gmail.com>
2025-11-13 22:52:05 -0300

    Apenas adicao do serializer instrutor

---
+++ SGEM/instrutores/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Instrutor
+
+class InstrutorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Instrutor
+        fields = '__all__'
+        read_only_fields = ['id_instrutor']
--- SGEM/instrutores/views.py
+++ SGEM/instrutores/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser 
 from .models import Instrutor
+from .serializers import InstrutorSerializer
 
 class InstrutorViewSet(viewsets.ModelViewSet):
     queryset = Instrutor.objects.all()
+    serializer_class = InstrutorSerializer
     permission_classes = [IsAdminUser]
